cea8b1da1ea5cfa429a7462a407f01e53d2fd3f0
zyhzxrlove <114069283+zyhzxrlove@users.noreply.github.com>
2022-09-22 14:19:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "蓉蓉",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "[oASwU5-1IWlCNEfCt8ZrBZ1wtW_s,oASwU5_mLjaoY2gGFi2tizlRdrAo]",
+      id: "oASwU5-1IWlCNEfCt8ZrBZ1wtW_s",
      
      
       // 你想对他发送的模板消息的模板ID
